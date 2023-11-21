018995939191782114257c486e25a07b7eb1824c
IlyaYakovlev <ilya.yakowlev@yandex.ru>
2023-11-21 10:50:55 +0300

    Возможность искать по классу дива
    Добавлено описание и примеры

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# webparser
+# Парсер веб-страницы
+
+## Описание
+
+Программа извлекает полезный текст из веб-страницы и сохраняет его в файл txt.
+Программа оформлена в виде утилиты командной строки, которой в качестве параметра
+указывается URL.
+
+Пример запуска: python main.py --url https://lenta.ru/news/2013/03/dtp/index.html
+
+В файле settings.ini задаются теги искомых элементов и классы, для элементов div.
+
+## Библиотеки
+
+argparse, configparser, urlparse, requests, BeautifulSoup
+
+## Результаты
+
+Список URL на которых проходило тестирование:
+- https://ria.ru/20231120/zagrazhdeniya-1910643784.html
+- https://lenta.ru/news/2023/11/21/belyy-dom-otvetil-na-vopros-ob-otkaze-ot-tehnologiy-spacex-posle-zayavleniy-maska/
+- https://www.gazeta.ru/business/news/2023/11/20/21751711.shtml
+
+Результаты находятся в папке examples.
+
+## Дальнейшее улучшение
+
+- Расширить возможность настройки для нахождения полезного текста.
+- Добавить настройки форматирования текста для выходного файла.
--- src/main.py
+++ src/main.py
@@ -6,23 +6,28 @@ import requests
 from bs4 import BeautifulSoup
 
 
+class Config:
+    # Получаем настройки из файла ini
+    __config = configparser.ConfigParser()
+    if __config.read('src/config/settings.ini'):
+        tags = __config.get('Webparser', 'tags', fallback='').split(',')
+        div_classes = __config.get('Webparser', 'div_classes', fallback='').split(',')
+    else:
+        tags = []
+        div_classes = []
+    if div_classes:
+        tags.append('div')
+
+
+cfg = Config()
+
+
 def get_url():
     # Получаем ссылку из аргументов
     parser = argparse.ArgumentParser(description='Webparser')
     parser.add_argument('--url', required=True, help='URL')
     args = parser.parse_args()
     return args.url
-
-
-class Config:
-    # Получаем настройки изи файла ini
-    __config = configparser.ConfigParser()
-    __config.read('settings.ini')
-    tags = __config['Webparser']['tags'].split(',')
-
-
-url = get_url()
-cfg = Config
 
 
 def split_into_lines(text):
@@ -44,12 +49,32 @@ def split_into_lines(text):
     return result
 
 
+def get_text_with_links(elem, dom):
+    # Получаем отформатированный текст с вставленными ссылками
+    for link in elem.find_all('a'):
+        link_text = link.text.strip()
+        link_href = link["href"]
+        if link_href.startswith('/'):
+            link_href = dom + link_href
+        link.replace_with(link_text + ' [' + link_href + ']')
+    return split_into_lines(elem.text.strip())
+
+
+def is_class_in_config(list_cls):
+    # Проверяем, что полученные классы div содержится в настройках
+    for cls in cfg.div_classes:
+        if cls in list_cls:
+            return True
+    return False
+
+
 def main():
+    url = get_url()
     # Получаем html из указанной ссылки
     r = requests.get(url)
     html = r.text
     # Используем BeautifulSoup для парсинга html
-    soup = BeautifulSoup(html, 'lxml')
+    soup = BeautifulSoup(html, 'html.parser')
 
     # Записываем заголовок
     title = soup.title
@@ -66,16 +91,8 @@ def main():
     # Пробегаем по ним получая текстовое содержимое
     elem_all = soup.find_all(cfg.tags)
     for elem in elem_all:
-        if elem.name.startswith('h'):
-            text += '\n\n' + split_into_lines(elem.text.strip())
-        else:
-            for link in elem.find_all('a'):
-                link_text = link.text.strip()
-                link_href = link["href"]
-                if link_href.startswith('/'):
-                    link_href = dom + link_href
-                link.replace_with(link_text + ' [' + link_href + ']')
-            elem_text = split_into_lines(elem.text.strip())
+        if elem.name != 'div' or elem.name == 'div' and elem.has_attr('class') and is_class_in_config(elem['class']):
+            elem_text = get_text_with_links(elem, dom)
             if len(elem_text) > 0:
                 text += '\n\n' + elem_text
 
